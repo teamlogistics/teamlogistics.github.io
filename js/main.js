c76716407e5f57a27014936f44c0bbca29c08590
anutka69 <31917118+anutka69@users.noreply.github.com>
2017-12-01 22:04:46 +0300

    calc_test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {
+
+function plus(){
+var num1, num2, num3, result;
+num1=document.getElementById('n1').value;
+num1=parseInt(num1);
+
+num2=document.getElementById('n2').value;
+num2=parseInt(num2);
+
+num3=document.getElementById('n3').value;
+num3=parseInt(num3);
+
+result = num1*num2*num3;
+
+document.getElementById('out').innerHTML=result;
 
 
 
-});
+}
